0dcabeb02d5086ae60b66c60ef9d252c26a44c1c
Neo Choi <neo.choi@gmail.com>
2016-05-02 02:32:22 +0800

    Follow AfterShip ESLint config in app.js

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # b6389e9e-000a-11e6-86aa-5e5517507c66
+Just need to open `index.html`
+
+## Tools used
+
+* webpack
+* jade
+* bootstrap 3
+* jquery
+* sass
+
+## Build
+First, you have to install the dependencies by `npm install`
+
+To build, use `npm start`, or `webpack`
--- src/js/app.js
+++ src/js/app.js
@@ -6,17 +6,24 @@ const main = require('../template/main.jade')();
 $('body').html(main);
 
 $('#SendMsgMessage').keypress(function (e) {
-  let msgLength = e.target.value.length;
-  if (msgLength >= 140) {
-    e.preventDefault();
-  }
+	let msgLength = e.target.value.length;
+	if (msgLength >= 140) {
+		e.preventDefault();
+	}
 }).keyup(function (e) {
-  let msgLength = e.target.value.length;
-  $('#SendMsgMessageCount').text(msgLength + '/140');
+	let msgLength = e.target.value.length;
+	$('#SendMsgMessageCount').text(msgLength + '/140');
 });
 
 $('.send-me-msg .btn').click(function (e) {
-  e.preventDefault();
-  $('#SendMsgSubject').val('');
-  $('#SendMsgMessage').val('').keyup();
+	e.preventDefault();
+	if ($('#SendMsgSubject').val().length === 0) {
+		$('#SendMsgMessageResponse').text('Please input subject.');
+	} else if ($('#SendMsgMessage').val().length === 0) {
+		$('#SendMsgMessageResponse').text('Please input your message.');
+	} else {
+		$('#SendMsgSubject').val('');
+		$('#SendMsgMessage').val('').keyup();
+		$('#SendMsgMessageResponse').text('Your message is sent.');
+	}
 });
